1be8080ad24f675562537d75c9be20badf451e94
Dave Fisher <dave.j.fish@gmail.com>
2022-08-08 19:47:10 -0700

    added secrets table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -10,3 +11,17 @@ CREATE TABLE users (
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL
 );
+
+CREATE TABLE secrets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO secrets 
+(title, description)
+VALUES
+('secret', 'gov secret'),
+('super secret', 'super gov secret'),
+('big secret', 'big gov secret');
